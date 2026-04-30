ff4f5e0dafbaf99709278aa8627aaefb6beb7419
Anh Le <anh.le.nvsu@gmail.com>
2026-04-30 14:02:58 -0400

    fix create booking, add create waitlist booking

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -9,3 +9,17 @@ export function camelToSnakeCase(str: string) {
 	}
 	return res;
 }
+
+export function validateOneHourBookTime(
+	startDate: string,
+	endDate: string,
+): boolean {
+	try {
+		const diffTime = Date.parse(endDate) - Date.parse(startDate);
+		if (diffTime < 3600) return false;
+		else return true;
+	} catch (err) {
+		console.log(`Date validation failed: ${err}`);
+		return false;
+	}
+}
