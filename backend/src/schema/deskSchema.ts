206f6c4ce10a4e429e657e0aae5adab25c572da4
Anh Le <anh.le.nvsu@gmail.com>
2026-05-01 18:16:12 -0400

    add update desk fn, add desk schema

--- backend/src/schema/deskSchema.ts
+++ backend/src/schema/deskSchema.ts
@@ -18,3 +18,23 @@ export const getAllDesksStartDateEndDate = z.object({
 	start_date: z.iso.time({ precision: -1 }),
 	end_date: z.iso.time({ precision: -1 }),
 });
+
+export const updateDeskSchema = z
+	.object({
+		id: z.int(),
+		name: z.string().optional(),
+		description: z.string().max(200).optional(),
+		start_hour: z.iso.time({ precision: -1 }).optional(),
+		end_hour: z.iso.time({ precision: -1 }).optional(),
+	})
+	.refine(
+		({ name, description, start_hour, end_hour }) =>
+			name || description || start_hour || end_hour,
+		{
+			error: "At least one of name, description, start_hour, end_hour.",
+		},
+	);
+
+export const deleteDeskSchema = z.object({
+	id: z.int(),
+});
