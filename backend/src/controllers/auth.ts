57765d0b405f756e32b65717c9d987b30a29d1f6
Anh Le <anh.le.nvsu@gmail.com>
2026-05-05 17:10:05 -0400

    fix sign up fn to support linking

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -24,26 +24,17 @@ export async function signUp(newUser: any) {
 		const { data, error } = await supabase.auth.signUp({
 			email: email,
 			password: password,
+			options: {
+				data: {
+					firstname: firstname,
+					lastname: lastname,
+				},
+			},
 		});
 		if (error) {
 			return returnOpFailed(JSON.stringify(error));
 		}
-		if (!data["user"] || !data["user"]["id"]) {
-			console.log(data);
-			return returnOpFailed("Could not get user id and information.");
-		}
-		const userAuthId = data["user"]["id"];
-		const newUserInfo = {
-			email: email,
-			password: password,
-			firstname: firstname,
-			lastname: lastname ?? null,
-			user_authid: userAuthId,
-		};
-		const createdUser = await sql`
-			INSERT INTO users ${sql(newUserInfo, "email", "password", "firstname", "lastname", "user_authid")}
-		`;
-		return returnSuccess(createdUser);
+		return returnSuccess(data);
 	} catch (err) {
 		return returnGeneralError(err);
 	}
